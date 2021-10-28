34f35d548d38cfd7a27f225cb900e6b0d8e117e6
Kevin Petow <kevin.m.petow@outlook.com>
2021-10-27 23:59:11 -0400

    tried finishing the calc assignment...

--- HTML-CSS-JS/Assignment 3 - Calculator/script.js
+++ HTML-CSS-JS/Assignment 3 - Calculator/script.js
@@ -1,43 +1,73 @@
+var firstNum;
+var secondNum;
+var operation;
+var defaultVal = 0;
+var result;
 
+var output = document.getElementById("output");
 
-// function getHistory(){
-//     return document.getElementById("history-value").innerText; //returns value of "history"
-// }
-// function printHistory(num){
-//     document.getElementById("history-value").innerText = num;
-// }  
-// function getOutput(){
-//     return document.getElementById("output-value").innerText;
-// }
-// function printOutput(num){
-//     if(num==""){
-//         document.getElementById("output-value").innerText=num;
-//     }
-//     else{
-//         document.getElementById("output-value").innerText = getFormattedNumber(num);
-//     }
-    
-// }
-// function getFormattedNumber(num){
-//     var n = Number(num);
-//     var value = n.toLocaleString("en");
-//     return value;
-// }
-// function reverseNumberFormat(num){
-//     return Number(num.replace(/,/g,''));
-// }
-// var operator = document.getElementsByClassName("operator");
-// for(var i=0; i<operator.length; i++){
-//     operator[i].addEventListener('click', function(){
-//     })
-// }
-// var digit = document.getElementsByClassName("digit");
-// for(var i=0; i<digit.length; i++){
-//     digit[i].addEventListener('click', function(){
-//         var output = reverseNumberFormat(getOutput());
-//         if(output != NaN){
-//             output = output + this.id;
-//             printOutput(output);
-//         }
-//     })
-// }
+function digit(number){
+     if(output.innerHTML == 0){
+         output.innerHTML = number;
+     }else{
+         output.innerHTML += number;
+    }
+    firstNum = output.innerHTML;
+    console.log(firstNum);
+}
+function logicDivide(){
+    output.innerHTML = output.innerHTML + "/";
+    document.getElementById("divide").disabled = true;
+    document.getElementById("multiply").disabled = true;
+    document.getElementById("subtract").disabled = true;
+    document.getElementById("add").disabled = true;
+    secondNum = output.innerHTML;
+    secondNum = secondNum.split("/").pop();
+}
+function logicMultiply(){
+    output.innerHTML = output.innerHTML + "*";
+    document.getElementById("divide").disabled = true;
+    document.getElementById("multiply").disabled = true;
+    document.getElementById("subtract").disabled = true;
+    document.getElementById("add").disabled = true;
+}
+function logicMinus(){
+    output.innerHTML = output.innerHTML + "-";
+    document.getElementById("divide").disabled = true;
+    document.getElementById("multiply").disabled = true;
+    document.getElementById("subtract").disabled = true;
+    document.getElementById("add").disabled = true;
+}
+function logicAdd(){
+    output.innerHTML = output.innerHTML + "+";
+    document.getElementById("divide").disabled = true;
+    document.getElementById("multiply").disabled = true;
+    document.getElementById("subtract").disabled = true;
+    document.getElementById("add").disabled = true;
+}            
+console.log(secondNum);
+
+function calcOutput(){
+    switch(){
+        case add:
+            output.innerHTML = firstNum + secondNum;
+            break;
+        case subtract:
+            output.innerHTML = firstNum - secondNum;
+            break;
+        case divide:
+            output.innerHTML = firstNum / secondNum;
+            break;
+        case multiply:
+            output.innerHTML = firstNum * secondNum;
+            break;
+    }
+}
+
+function clearData(){
+    output.innerHTML = defaultVal;
+    document.getElementById("divide").disabled = false;
+    document.getElementById("multiply").disabled = false;
+    document.getElementById("subtract").disabled = false;
+    document.getElementById("add").disabled = false;
+}
