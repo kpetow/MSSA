518d2754f6505e348e1479aa3371744f1516f551
Kevin Petow <kevin.m.petow@outlook.com>
2021-11-17 21:56:21 -0500

    added two files for WPF lessons

--- CS/WPF Controls Challenge/WPF Controls Challenge/MainWindow.xaml.cs
+++ CS/WPF Controls Challenge/WPF Controls Challenge/MainWindow.xaml.cs
@@ -12,25 +12,117 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace WPF_Controls_Challenge
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    public class Coffee
+    {
+        public string size { get; set; }
+        public string item { get; set; }
+        public string extras { get; set; }
+        public string output { get; set; }
+
+    }
     public partial class MainWindow : Window
     {
+        string size;
+        string item;
+        string extras;
+        string summary;
+
+        Coffee coffee1 = new Coffee();
+       
+        
+
         public MainWindow()
         {
             InitializeComponent();
             //btnOrder_Click();
+            this.DataContext = coffee1;
+        }
+
+        public void summaryUpdate()
+        {
+            this.summary = $"{size} {item} {extras}";
+            coffee1.output = summary;
+            Debug.WriteLine(coffee1.output);
+            //summaryText.Text = summary;
         }
 
         private void btnOrder_Click(object sender, RoutedEventArgs e)
         {
-            //string order = "latte";
-            //string size = "medium";
-            //OrderOutput.Text = $"{size} {order}";
+            if (sender.Equals(sizeSmall))
+            {
+                this.size = "Small";
+            }
+            if (sender.Equals(sizeMedium))
+            {
+                this.size = "Medium";
+            }
+            if (sender.Equals(sizeLarge))
+            {
+                this.size = "Large";
+            }
+            summaryUpdate();
+        }
+
+        private void menuSelection(object sender, RoutedEventArgs e)
+        {
+            if (sender == Latte)
+            {
+                this.item = "Latte";
+            }
+            if (sender == Frapuccino)
+            {
+                this.item = "Frapuccino";
+            }
+            if (sender == Mocha)
+            {
+                this.item = "Mocha";
+            }
+            if (sender == Machiato)
+            {
+                this.item = "Machiato";
+            }
+            summaryUpdate();
+        }
+
+        private void addExtras(object sender, RoutedEventArgs e)
+        {
+            if (Cream.IsChecked == true && Sugar.IsChecked == false)
+            {
+                this.extras = "with Cream";
+            }
+            if (Sugar.IsChecked == true && Cream.IsChecked == false)
+            {
+                this.extras = "with Sugar";
+            }
+            if (Cream.IsChecked == true && Sugar.IsChecked == true)
+            {
+                this.extras = "with Cream and Sugar";
+            }
+            if (Cream.IsChecked == false && Sugar.IsChecked == false)
+            {
+                this.extras = "";
+            }
+            summaryUpdate();
+        }
+
+        private void submitOrder(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Order Submitted!");
+            Sugar.IsChecked = false;
+            Cream.IsChecked = false;
+            sizeSmall.IsChecked = false;
+            sizeMedium.IsChecked = false;
+            sizeLarge.IsChecked = false;
+            menuItems.SelectedIndex = -1;
+            summaryText.Text = String.Empty;
         }
     }
 }
---
+++ CS/WPF-Loadouts/WPF-Loadouts/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace WPF_Loadouts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Loadout
+        {
+            public int brightness { get; set; }
+            public bool easy { get; set; }
+            public bool normal { get; set; }
+            public bool hard { get; set; }
+            public string resolution { get; set; }
+            public bool hints { get; set; }
+
+
+            public Loadout()
+            {
+                this.brightness = 50;
+                this.easy = false;
+                this.normal = true;
+                this.hard = false;
+                this.resolution = "1920 x 1080";
+                this.hints = false;
+
+            }
+        }
+
+        Loadout loadout1 = new Loadout();
+        Loadout loadout2 = new Loadout();
+        Loadout loadout3 = new Loadout();
+        Loadout loadout4 = new Loadout();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void load1(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = loadout1;
+            btn1.Background = new SolidColorBrush(Color.FromRgb(100, 175, 200));
+            btn2.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+            btn3.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+            btn4.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+            
+            
+        }
+        private void load2(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = loadout2;
+            btn2.Background = new SolidColorBrush(Color.FromRgb(100, 175, 200));
+            btn1.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+            btn3.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+            btn4.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+
+        }
+        private void load3(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = loadout3;
+            btn3.Background = new SolidColorBrush(Color.FromRgb(100, 175, 200));
+            btn2.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+            btn1.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+            btn4.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+
+        }        
+        private void load4(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = loadout4;
+            btn4.Background = new SolidColorBrush(Color.FromRgb(100, 175, 200));
+            btn2.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+            btn3.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+            btn1.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200));
+
+        }
+
+        private void printValue(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Debug.WriteLine($"Loadout 1 brightness = {loadout1.brightness}");
+            Debug.WriteLine($"Loadout 2 brightness = {loadout2.brightness}");
+        }
+    }
+}
